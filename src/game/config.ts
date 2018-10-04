5aabe78b7ca8fe33143cad3ba83f4f557681e107
Jonathan Fransson <jonathan2.fransson@gmail.com>
2018-10-04 16:17:17 +0200

    added scene for combat

--- src/game/config.ts
+++ src/game/config.ts
@@ -1,20 +1,22 @@
 import { GameScene } from '@/scenes/GameScene';
+import { FightScene, BattleScene, UIScene } from '@/scenes/FightScene';
 
 /**
  * Phaser game config
  */
 export const phaserConfig: GameConfig = {
-  parent: 'app',
-  type: Phaser.AUTO,
-  scene: [GameScene],
-  width: 1024,
-  height: 576,
+  parent: 'app', // content??
+  type: Phaser.AUTO, // AUTO
+  scene: [FightScene, BattleScene, UIScene], // GameScene
+  width: 320, // 1024
+  height: 240, // 576
+  zoom: 2,
 
   // Sample config items
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 300 }
+      gravity: { y: 0 }
     }
   },
 };
