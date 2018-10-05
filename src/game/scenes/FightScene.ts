b31b5b58d2413c89920e9731408f499a89933d5d
Jonathan Fransson <jonathan2.fransson@gmail.com>
2018-10-05 12:50:16 +0200

    added units and temporary sprites

--- src/game/scenes/FightScene.ts
+++ src/game/scenes/FightScene.ts
@@ -4,6 +4,44 @@ import { BlendModes } from 'phaser';
  * Sample Phaser scene
  * @extends {Phaser.Scene}
  */
+
+export class Unit extends Phaser.GameObjects.Sprite {
+
+  public constructor(x, y, texture, frame, type, hp, damage) {
+    super(x, y, texture, frame, type, hp, damage);
+    // Phaser.GameObjects.Sprite.call(this, this.scene, x, y, texture, frame);
+    this.type = type;
+    this.maxHp = this.hp = hp;
+    this.damage = damage; // default damage
+  }
+
+  public attack(target:number): void { // number???
+    target.takeDamage(this.damage);
+  }
+  public takeDamage(damage:number): void {
+    this.hp -= damage;
+  }
+
+}
+
+export class Enemy extends Unit {
+  public constructor(scene, x, y, texture, frame, type, hp, damage) {
+    // Unit.call(this, scene, x, y, texture, frame, type, hp, damage);
+
+    super(scene, x, y, texture, frame, type, hp, damage);
+  }
+}
+
+export class PlayerCharacter extends Unit {
+  public constructor(scene, x, y, texture, frame, type, hp, damage) {
+    // Unit.call(this, scene, x, y, texture, frame, type, hp, damage);
+    // glöm inte att flippa sprite osv
+    // this.flipX = true;
+    // this.setScale(2);
+    super(scene, x, y, texture, frame, type, hp, damage);
+  }
+}
+
 export class FightScene extends Phaser.Scene {
   /**
    * Phaser preload method
@@ -12,12 +50,17 @@ export class FightScene extends Phaser.Scene {
    */
   public preload(): void {
     console.debug('Preload');
+    this.load.image('player', 'assets/FaceMan.png');
+    this.load.image('enemy', 'assets/FaceDude.png');
+
     // Fixa lite nice sprites
     // player enemy osv
   }
 
   public constructor() {
     super({ key: 'FightScene' });
+    // Phaser.Scene.call(this, { key: 'Fightscene' });
+
   }
 
   /**
@@ -37,14 +80,29 @@ export class BattleScene extends Phaser.Scene {
 
   public constructor() {
     super({ key: 'BattleScene' });
+    // Phaser.Scene.call(this, { key: 'BattleScene' });
+
+    // super();
   }
 
   public create(): void {
     console.debug('Create');
-
-    this.scene.launch('UIScene');
-
     this.cameras.main.setBackgroundColor('rgba(0, 200, 0, 0.5)');
+
+    const warrior = new PlayerCharacter(this, 250, 50, 'player', null, 'warrior', 100, 20);
+    this.add.existing(warrior);
+
+    const faceDude = new Enemy(this, 50, 50, 'enemy', null, 'faceDude', 50, 3);
+    this.add.existing(faceDude);
+
+    this.heroes = [warrior];
+
+    this.enemies = [faceDude];
+
+    this.units = this.heroes.concat(this.enemies);
+
+    // Run UI scene at the same time
+    this.scene.launch('UIScene');
   }
 
 }
